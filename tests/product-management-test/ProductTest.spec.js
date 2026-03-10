543ae9354eecb581313dea3f00f214133ef6f4d9
Aromal100 <aromal.resbee@gmail.com>
2026-03-10 19:00:08 +0530

    he

--- tests/product-management-test/ProductTest.spec.js
+++ tests/product-management-test/ProductTest.spec.js
@@ -4,18 +4,18 @@ import {AdminLoginPage} from '../../pages/login-pages/AdminLoginPage.js';
 import  {SidePages} from '../../pages/common-pages/SidePages.js';
 const datas= JSON.parse(JSON.stringify(require('../../config/testData.json')));
 
-test.beforeEach(async ({ page }) => {
-  const adminLoginPage = new AdminLoginPage(page);
-  await adminLoginPage.landing();
-  await adminLoginPage.adminlogin(datas.username, datas.password);
-});
+// test.beforeAll(async ({ page }) => {
+//   const adminLoginPage = new AdminLoginPage(page);
+//   await adminLoginPage.landing();
+//   await adminLoginPage.adminlogin(datas.username, datas.password);
+// });
 
 test('Adding the uniform product', async ({ page }) => {
 
-    // const pp = new ProductPage(page);
-    // const adminLoginPage = new AdminLoginPage(page);
-    // await adminLoginPage.landing();
-    // await adminLoginPage.adminlogin(datas.username, datas.password);
+    const pp = new ProductPage(page);
+    const adminLoginPage = new AdminLoginPage(page);
+    await adminLoginPage.landing();
+    await adminLoginPage.adminlogin(datas.username, datas.password);
     const sp= new SidePages(page);
     await sp.openProductPage();
     await pp.addUniformProduct("Uniform", "25");
@@ -25,6 +25,10 @@ test('Adding the uniform product', async ({ page }) => {
 })
 
 test('Editing the product',async({page})=>{
+
+    const adminLoginPage = new AdminLoginPage(page);
+  await adminLoginPage.landing();
+  await adminLoginPage.adminlogin(datas.username, datas.password);
     
     const sp= new SidePages(page);
     await sp.openProductPage();
