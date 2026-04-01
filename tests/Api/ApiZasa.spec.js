94c6e12d65f6ff414a6a146ec5f622fdbb6b491a
Aromal100 <aromal.resbee@gmail.com>
2026-04-01 11:47:43 +0530

    post

--- tests/Api/ApiZasa.spec.js
+++ tests/Api/ApiZasa.spec.js
@@ -1,34 +1,79 @@
-import {test,expect} from '@playwright/test'
+import { test, expect } from '@playwright/test'
 
 
-test('Api ui test ',async({page,request})=>{
+test('Api ui test ', async ({ page, request }) => {
 
-    const req=await request.get("https://api.demoblaze.com/entries");
-    const body = await req.json();  
-    console.log(body.Items[0].title);
+    const req = await request.get("https://api.demoblaze.com/entries");
+
+    const body = await req.json();
+    console.log(body.Items[1].title);
 
     await page.goto("https://demoblaze.com/");
-    await expect(page.getByRole('link',{name:'Samsung galaxy s6'})).toHaveText(body.Items[0].title);
+    await expect(page.getByRole('link', { name: 'Nokia lumia 1520' })).toHaveText(body.Items[1].title);
 
 })
 
-test.only('Post Api Testing',async({request})=>{
 
-   const postapi=await request.post("/booking",{
-    data:{
-         "firstname": "Jim",
-        "lastname": "Brown",
-        "totalprice": 111,
-        "depositpaid": true,
-        "bookingdates": {
-            "checkin": "2018-01-01",
-            "checkout": "2019-01-01"
-        },
-        "additionalneeds": "Breakfast"
-    }
-   })
-   const res= await postapi.json();
-   console.log(res);
+test('Api test with post method', async ({ page, request }) => {
 
+    const req = await request.post("/booking", {
+        data:
+        {
+            "firstname": "Jim",
+            "lastname": "Brown",
+            "totalprice": 111,
+            "depositpaid": true,
+            "bookingdates": {
+                "checkin": "2018-01-01",
+                "checkout": "2019-01-01"
+            },
+            "additionalneeds": "Breakfast"
+        }
+    })
+
+    const jsonRes = await req.json();
+
+    console.log(jsonRes);
+    expect(jsonRes.booking).toMatchObject({
+        firstname: 'Jim',
+        lastname: 'Brown',
+        totalprice: 111,
+        depositpaid: true,
+        bookingdates: { checkin: '2018-01-01', checkout: '2019-01-01' },
+        additionalneeds: 'Breakfast'
+    })
 
 })
+
+test.only('Api post on BM store', async ({ request }) => {
+
+
+       const loginRes=await request.post("https://storeapi-qa.globtech.in/api_fe/signin",{
+        data:{
+        "email":"arshadazadad@yahoo.com","password":"123456"
+        }
+       });
+
+       const loginBody=await loginRes.json();
+       const token=loginBody.response.token;
+       
+
+
+
+        const newReq =await request.post("https://storeapi-qa.globtech.in/api_fe/save_product_in_cart",{
+            headers:{
+                Authorization: `Bearer ${token}`
+            },
+            data: {
+                "school_id": "4", "student_id": "652", "house": "mystique", "user_id": 434, "product_id": 1845, 
+                "product_name": "Pe Girls Top Mystique NIMS", "amount": 75, "quantity": 1, "total_amount": 75, 
+                "category": "uniform", "type": "single", "size_id": 103
+            }
+        });
+
+        const res=await newReq.json();
+        console.log(res);
+      
+
+
+    })
